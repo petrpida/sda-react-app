c6e9d23e8595a4a7b49cf487e105b9acc198e221
petrpida <petstu@email.cz>
2022-03-13 11:43:30 +0100

    5. counter added

--- src/sharedComponents/MyButton/index.jsx
+++ src/sharedComponents/MyButton/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Button({ buttonName }) {
+  let [counter, setCounter] = useState(0)
+  console.log(counter)
+  
+
   return (
-    <button
+    <div><button
       onClick={function () {
-        window.alert((buttonName) + " was just clicked");
+        //window.alert((buttonName) + " was just clicked");
+        counter = counter + 1
+        // console.log(counter)
+        setCounter(counter)
       }}
     >
       {buttonName}
     </button>
+    <p>na {buttonName} bylo kliknuto {counter}x</p>
+    </div>
   );
 }
