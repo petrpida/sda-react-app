af80f3aa6a674e7e144a6d5726016c03776ebfcb
petrpida <petstu@email.cz>
2022-03-13 09:33:44 +0100

    new component "MyButton" added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Game from './sharedComponents/TicTacToe/Game';
+// import Game from './sharedComponents/TicTacToe/Game';
 // import Menu from './sharedComponents/Menu';
+import Button from "./sharedComponents/MyButton";
 
 function App() {
   return (
     <div>
       {/* <Menu menuName="Menu SDA" menuItems={["odkaz1", "odkaz2", "odkaz3"]} /> */}
-      <Game gameName="PISKVORKY" players={2} />
+      {/* <Game gameName="PISKVORKY" players={2} /> */}
+      <Button />
     </div>
   );
 }
