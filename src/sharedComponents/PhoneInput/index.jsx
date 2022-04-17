f53a6cde23cd819675bc8542578f19dd8dce7197
petrpida <petstu@email.cz>
2022-04-17 23:42:53 +0200

    phoneInput done

--- src/sharedComponents/PhoneInput/index.jsx
+++ src/sharedComponents/PhoneInput/index.jsx
@@ -12,15 +12,24 @@ export default function PhoneInput() {
                 setInputValue(e.target.value)
             }} />
             <button  type="submit" onClick={function() {
-                fetch(`https://api.veriphone.io/v2/verify?phone=${inputValue}&key=9E2ABBC635534240938B1886027F3376`)
+                fetch(`https://api.veriphone.io/v2/verify?phone=${inputValue}&key=F7C3EFC4BCD341E78BDDDA310CD0D2EB`)
                 .then((response) => response.json())
                 .then((responseData) => {
+                   console.log(responseData) 
                    setData(responseData)
                 })
             }}>validate</button>
 
-            {/* {data.phone_valid && <h6>This is valid phone number</h6>}  
-            {!data.phone_valid && <h6>This is NOT valid phone number</h6>}          */}
+            {data && data.phone_valid && 
+            <div>
+            <h4>This is valid phone number</h4>
+            <p>Country: {data.phone_region}</p>
+            <p>Phone type: {data.phone_type}</p>
+            <p>Carrier: {data.carrier}</p>
+
+            </div>
+            }  
+            {data && !data.phone_valid && <h4>This is NOT valid phone number</h4>}         
 
             <NavButton name="home" link="/"/>
         </div>
